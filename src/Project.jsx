f46b4d5626ad988550d19f96b885822c0d20b912
Wanting(Christina) You <157079307+Yukieos@users.noreply.github.com>
2025-05-18 22:25:43 -0400

    Update Project.jsx

--- src/Project.jsx
+++ src/Project.jsx
@@ -7,40 +7,43 @@ const Project = () => {
       
       <div className="project-item">
         <div className="project-header">
-          <h3>My GitHub: <a href="https://github.com/Yukieos" target="_blank" rel="noopener noreferrer">https://github.com/Yukieos</a></h3>
+          <h2>My GitHub: <a href="https://github.com/Yukieos" target="_blank" rel="noopener noreferrer">https://github.com/Yukieos</a></h2>
+          <h3>Personal Project</a></h3>
+          <h4>Photo‑Searched Price Comparison App (AWS/Swift UI/PyTorch/PostgreSQL)</h4>
           <span className="location">New York, Current</span>
         </div>
         <ul>
-          <li>Building an interactive chatbox on Hugging Face Spaces using Gradio, Gemma LLM, and Transformers, enabling immersive character-based roleplay for creative writing and fanfiction.</li>
-          <li>Implementing a customizable character creation system with flexible identity, personality traits, and language styles paired with a responsive chat interface featuring emotional style selection.</li>
-          <li>Engineering prompt templates that effectively combine persona information, conversation history, and emotional styles while maintaining a memory system to track emotional arcs across interactions.</li>
-          <li>Building a content moderation pipeline with user feedback collection system, positioning the demo as a unique showcase on Hugging Face that highlights Gemma's capabilities for emotional intelligence and roleplay.</li>
+          <li>Captured images and implemented automated cropping, resizing, and color normalization pipelines to ensure consistent model‑inference inputs.</li>
+          <li>Deployed MobileNet V3 Small for object classification(88.1% accuracy), and incorporated EasyOCR for packaging‑text extraction(reach F1 81.8%)</li>
+          <li>Trained PyTorch model: https://colab.research.google.com/drive/11WNbGK8rpkXbFgYFW4iNv9SRYmt4SVzu?usp=sharing </li>
+          <li>Collected price data from Amazon Fresh, Whole Foods, Trader Joe’s, Weee, Costco, stored in AWS RDS (PostgreSQL); SQL views return unit- and total-price rankings.</li>
         </ul>
       </div>
 
       <div className="project-item">
         <div className="project-header">
-          <h3>Restaurant Reservation System</h3>
-          <span className="location">New York, 2023/5</span>
+          <h3>Columbia University COMSW4111 Database -Individual Project</h3>
+          <h4>Restaurant Reservation System(Flask, PostgreSQL, GCP VM, Jinja, CSS)</h4>
+          <span className="location">New York, Feb 2025-Apr 2025</span>
         </div>
-        <p>Flask, PostgreSQL, OCR, VM, Nginx, CSS</p>
         <ul>
-          <li>Built and deployed restaurant booking system with user functions, menu ordering, booking and checking reservation features.</li>
-          <li>Designed and operated a relational database using PostgreSQL, integrated vis OCR Alchemy.</li>
-          <li>Applied text processing and data visualization for grocery booking and demand prediction.</li>
+          <li>Built a full-stack restaurant booking system with user login, menu/rating browsing, booking and checking reservations features.</li>
+          <li>Designed and queried a relational database using PostgreSQL, integrated via SQLAlchemy.</li>
+          <li>Deployed the app on a Google Cloud VM for remote testing and demonstration.</li>
+          <li>Website: http://34.75.98.206:8111/ </li>
         </ul>
       </div>
 
       <div className="project-item">
         <div className="project-header">
-          <h3>Restaurant Recommendation System</h3>
+          <h3>Columbia University MATH UN2015 Linear Algebra and Probability - Group Project(Team Leader)</h3>
+          <h4>Restaurant Recommendation System(Python, Linear Optimization)</h4>
           <span className="location">New York, 2024/11</span>
         </div>
-        <p>Python, Linear Optimization</p>
         <ul>
-          <li>Applied content-based filtering (cosine similarity) to recommend restaurants based on location and cuisine types.</li>
+          <li>Applied content-based filtering (cosine similarity) to recommend restaurants based on location and price range.</li>
           <li>Used Markov Chains/Singular Value Decomposition to predict user dining preferences and improve personalized suggestions.</li>
-          <li>Retrieved and processed Yelp API data of 2,000 Manhattan restaurants to generate real-world recommendations.</li>
+          <li>Retrieved and processed Yelp API data of 240 Manhattan restaurants to generate real-world recommendations.</li>
         </ul>
       </div>
     </div>
